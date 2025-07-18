b57ee4b0cbf9b6e6e396d870f983442f648efc93
l2q <bigtooth2006@gmail.com>
2025-07-18 16:52:18 +0800

    配置数据读写

--- include/Config.h
+++ include/Config.h
@@ -1,60 +1,39 @@
-// Config.h
+/*
+ * @Description:
+ * @Author: l2q
+ * @Date: 2025/07/18 15:30
+ * @LastEditors: l2q
+ * @LastEditTime: 2025/07/18 15:30
+ *
+ */
 
-#ifndef _CONFIG_h
-#define _CONFIG_h
+#ifndef ESP_UTILITY_CONFIG_H
+#define ESP_UTILITY_CONFIG_H
 
-#include <Ticker.h>
-#include <pb_encode.h>
-#include <pb_decode.h>
+#include "../../../include/config.pb.h"
+#include "precompiled.h"
+
 #include <pb.h>
-#include "Arduino.h"
-#include "GlobalConfig.pb.h"
+#include <pb_decode.h>
+#include <pb_encode.h>
 
-#if PB_PROTO_HEADER_VERSION != 30
-#error Regenerate this file with the current version of nanopb generator.
-#endif
-
-#define GLOBAL_CFG_VERSION 1 // 1 - 999
-
-//#define WIFI_SSID "qlwz"     // WiFi ssid
-//#define WIFI_PASS "" // WiFi 密码
-
-//#define MQTT_SERVER "10.0.0.25"   // MQTTַ 地址
-//#define MQTT_PORT 1883            // MQTT 端口
-//#define MQTT_USER "mqtt"          // MQTT 用户名
-//#define MQTT_PASS "" // MQTT 密码
-
-#define MQTT_FULLTOPIC "%module%/%hostname%/%prefix%/" // MQTT 主题格式
-
-#define OTA_URL "http://10.0.0.50/esp/%module%.bin"
-
-#define WEB_LOG_SIZE 4000  // Max number of characters in weblog
-#define BOOT_LOOP_OFFSET 5 // 开始恢复默认值之前的引导循环数 (0 = disable, 1..200 = 循环次数)
-
-extern char UID[16];
-extern char tmpData[512];
-extern GlobalConfigMessage globalConfig;
-extern uint32_t perSecond;
-extern Ticker *tickerPerSecond;
-
-class Config
+class config
 {
-private:
-    static uint16_t nowCrc;
-    static bool isDelay;
-    static uint8_t countdown;
+    SINGLE_TPL(config)
 
 public:
-    static uint16_t crc16(uint8_t *ptr, uint16_t len);
+    bool load();
+    bool save();
+    bool reset();
 
-    static void readConfig();
-    static void resetConfig();
-    static bool saveConfig(bool isEverySecond = false);
-    static void delaySaveConfig(uint8_t second);
+private:
+    uint16_t crc16(uint8_t* ptr, uint16_t len);
 
-    static void moduleReadConfig(uint16_t version, uint16_t size, const pb_field_t fields[], void *dest_struct);
-    static bool moduleSaveConfig(uint16_t version, uint16_t size, const pb_field_t fields[], const void *src_struct);
+public:
+    ConfigMessage _config;
 
-    static void perSecondDo();
+private:
+    uint16_t _crc;
 };
-#endif
+
+#endif  // ESP_UTILITY_CONFIG_H
---
+++ include/precompiled.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2012 YuanHang Inc. All rights reserved.
+ *
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND YuanHang HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * @Description: Public Macro Definition
+ * @Author: l2q
+ * @Date: 2012/3/8 13:27
+ * @LastEditors: lucky
+ * @LastEditTime: 2023/4/7 8:15
+ */
+
+#ifndef ESP_UTILITY_PRECOMPILED_H
+#define ESP_UTILITY_PRECOMPILED_H
+
+#include <Arduino.h>
+
+#ifndef UNUSED
+#define UNUSED(x) (void)x;
+#endif
+
+#ifndef SINGLE_TPL
+#define SINGLE_TPL(className)                               \
+private:                                                    \
+    className() { create(); }                               \
+    ~className() { destory(); }                             \
+                                                            \
+public:                                                     \
+    className(const className&)                   = delete; \
+    className&        operator=(const className&) = delete; \
+    static className& getInstance()                         \
+    {                                                       \
+        static className _instance;                         \
+        return _instance;                                   \
+    }                                                       \
+    void create();                                          \
+    void destory();
+#endif
+
+#define Print(fmt, ...) Serial.printf("[%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+#ifndef MAX_PATH
+#define MAX_PATH 260
+#endif
+
+#define GLOBAL_CFG_VERSION 1
+
+#endif  // ESP_UTILITY_PRECOMPILED_H
